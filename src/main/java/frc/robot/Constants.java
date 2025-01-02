eebe0b8f7ab32a39da60847311a1e494a9bbc667
Technados <117394920+Technados@users.noreply.github.com>
2025-01-02 16:34:09 -0500

    added libraries

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import com.revrobotics.CANSparkBase.IdleMode;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -16,6 +18,11 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+
+  public static final class NeoConstants {
+    public static final int kNeoMotorCanId = 11;
+    public static final IdleMode kNeoIdleMode = IdleMode.kBrake;}
+
 }
 
 // test comment for github
