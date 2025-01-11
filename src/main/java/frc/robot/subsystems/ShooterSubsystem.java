f8bf8266415d60f533057dbed9ca8fded33e2454
Technados <117394920+Technados@users.noreply.github.com>
2025-01-10 21:03:57 -0500

    TRYING MY BEST

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -9,43 +9,48 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.SparkPIDController;
 import com.revrobotics.CANSparkBase.ControlType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 // This Subsystem will act as a Shooter Subsystem with Volatge Compensation
 
 public class ShooterSubsystem extends SubsystemBase {
+    private static final double kP = 0.1;
+    private static final double kI = 0.001;
+    private static final double kD = 1.0;
+    private static final double kFF = 45;
     private final CANSparkMax ShooterMotor;
     private final RelativeEncoder ShooterEncoder;
     private final SparkPIDController pidController;
 
     public ShooterSubsystem() {
-        ShooterMotor = new CANSparkMax(3, MotorType.kBrushless);
+        ShooterMotor = new CANSparkMax(Constants.ShooterConstants.kShooterMotorCanId, MotorType.kBrushless);
         ShooterEncoder = ShooterMotor.getEncoder();
         SparkPIDController ShooterPIDController = ShooterMotor.getPIDController();
-
-        //Define PID coeficiants
-        double kP = 0.1;
-        double kI = 0.0; 
-        double kD= 0.0; 
-    //Tune values later
 
         // Set PID coeficiants
        pidController.setP(kP);
        pidController.setI(kI);
        pidController.setD(kD);
+       pidController.setFF(kFF);
+    }
 
-    double targetVelocityRPM = 4500.0; 
-    //Make desired RPM
+    public void setShooterVelocity(double targetVelocityRPM){
         pidController.setReference(targetVelocityRPM, CANSparkMax.ControlType.kVelocity);
+    } 
+
+    public void log() {
+        SmartDashboard.putNumber("ShooterVelocity", ShooterEncoder.getVelocity());
+    }
     //private static ShooterSubsystem instance;
 
     
         
-        ShooterMotor.setSmartCurrentLimit(30);
-        ShooterMotor.enableVoltageCompensation(12.0);
+        //ShooterMotor.setSmartCurrentLimit(30);
+        //ShooterMotor.enableVoltageCompensation(12.0);
     
 
     // Initialize motor
-    CANSparkMax ShooterMotor = new CANSparkMax(Constants.ShooterConstants.kShooterMotorCanId, MotorType.kBrushless);
+    //CANSparkMax ShooterMotor = new CANSparkMax(Constants.ShooterConstants.kShooterMotorCanId, MotorType.kBrushless);
     
     
    
@@ -53,28 +58,28 @@ public class ShooterSubsystem extends SubsystemBase {
     //private float neospeed = 0.30f;
 
    // public ShooterSubsystem(){
-      ShooterMotor.restoreFactoryDefaults();
-      ShooterMotor.setInverted(false);
-      ShooterMotor.setIdleMode(Constants.ShooterConstants.kShooterIdleMode);
+     // ShooterMotor.restoreFactoryDefaults();
+      //ShooterMotor.setInverted(false);
+      //ShooterMotor.setIdleMode(Constants.ShooterConstants.kShooterIdleMode);
      // ShooterMotor.setSmartCurrentLimit(30);
       //ShooterMotor.enableVoltageCompensation(12.0);
-      ShooterMotor.burnFlash();
+      //ShooterMotor.burnFlash();
 
       //ShooterEncoder.setPosition(0);
 
     
   
     //PID setup
-    pidController = ShooterMotor.getPIDController();
-    pidController.setP(kP);
-    pidController.setI(kI);
-    pidController.setD(kD);
+    //pidController = ShooterMotor.getPIDController();
+    //pidController.setP(kP);
+    //pidController.setI(kI);
+    //pidController.setD(kD);
     //pidController.setFF(kF);
     //FF stands for FreeForward this helps maintain the speed
-    }
+    //}
   // Spin ShooterMotor forward
   public void startShooterMotor(){
-    pidController.setReference(targetVelocityRPM, ControlType.kVelocity);
+    pidController.setReference(kFF, ControlType.kVelocity);
     //Maintain target RPM
   }
 
@@ -125,6 +130,7 @@ public class ShooterSubsystem extends SubsystemBase {
   //public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
   }
+
 
 
 
